cbb71ed96c337596fca22c3edac0e332b7bfd7c2
caroline.nalubega@andela.com <caroline.nalubega@andela.com>
2019-04-16 00:19:26 +0300

    feat(setup): Setup Relay
    Update dependencies
    Setup relay environment
    Setup plugins
    Setup relay-compiler
    Setup Query Renderer
    
    [Finishes #165274325]

--- src/app.js
+++ src/app.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from "./styles.css";
+import {graphql, QueryRenderer} from 'react-relay';
+import environment from './environment'
 
-const hello = <h1>Hello, react!</h1>;
+
+const hello =   <QueryRenderer
+environment={environment}
+query={graphql`
+query appQuery {
+  allCinemaDetails {
+    edges {
+      node {
+        id
+        slug
+        hallName
+      }
+    }
+  }
+}
+`}
+
+render={({error, props}) => {
+  if (error) {
+    return <div>Error!</div>
+  }
+  else if (props) {
+    return <h1>hello, react</h1>
+  } 
+  return <div>Loading...</div>
+}}
+/>
 ReactDOM.render(
   hello,
   document.getElementById('root')
