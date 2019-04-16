b7f8c3f6d77c32314a2fb24af3fb00ad19aaf587
kisakyegordon <gordon56692@gmail.com>
2019-04-16 21:24:15 +0300

    feat(design theme): adds basic theme layout

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import * as mockData from '../../../assets/mockData.js';
 
 const Dashboard = () => (
     <div>
         <h2>Main Content</h2>
-
     </div>
 );
 
--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import logo from '../../../public/images/logo.png';
 import { Form, FormGroup, Button} from 'reactstrap';
 import  './topbar.scss';
 
