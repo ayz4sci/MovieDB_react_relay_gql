f1e17d9157c066ff8d89075d5f711534c7017697
kisakyegordon <gordon56692@gmail.com>
2019-04-16 20:59:22 +0300

    feat(design theme): adds basic theme layout

---
+++ src/components/filter/filter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Form, FormGroup, Input, Label} from 'reactstrap';
+import './filter.scss';
+
+const genres = ['drama', 'family', 'horror'];
+const Filter = () => (
+    <div>
+        <h2> Filters </h2>
+
+    </div>
+)
+
+export default Filter;
