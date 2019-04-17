e2dfa06758e25c5b61d843ec731b165fecdf29fb
kisakyegordon <gordon56692@gmail.com>
2019-04-17 11:36:37 +0300

    feat(design theme): add scss configuration

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Form, FormGroup, Input, Label} from 'reactstrap';
+import {
+  Form, FormGroup, Input, Label
+} from 'reactstrap';
 import './filter.scss';
 
 const Filter = () => (
-    <div>
-        <h2> Filters </h2>
-    </div>
-)
+  <div>
+    <h2> Filters </h2>
+  </div>
+);
 
 export default Filter;
--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { Form, FormGroup, Button} from 'reactstrap';
-import  './topbar.scss';
+import { Form, FormGroup, Button } from 'reactstrap';
+import './topbar.scss';
 
 const TopBar = () => (
-    <div className="topbar">
+  <div className="topbar">
 
-        <div className='topbar-content'>
-            <div className="logo">
-                <img src='../../../assets/images/logo.png' alt='logo' />
-            </div>
-            <div className='search-form'>
-                <Form>
-                    <FormGroup>
-                        <input type='text' name='search' placeholder='search for movie, title, year '  />
-                        <Button type='submit' size='sm' color='primary'>Search</Button>
-                    </FormGroup>
-                </Form>
-            </div>
-        </div>
+    <div className="topbar-content">
+      <div className="logo">
+        <img src="../../../assets/images/logo.png" alt="logo" />
+      </div>
+      <div className="search-form">
+        <Form>
+          <FormGroup>
+            <input type="text" name="search" placeholder="search for movie, title, year " />
+            <Button type="submit" size="sm" color="primary">Search</Button>
+          </FormGroup>
+        </Form>
+      </div>
     </div>
-)
+  </div>
+);
 
 export default TopBar;
