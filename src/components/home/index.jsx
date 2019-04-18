5fbdde1990cb1c44cb95b44af5725417305e0d21
Ayoola Ajebeku <ayz4sci@yahoo.com>
2019-04-18 16:27:49 +0100

    feat(search): Implements search by job title

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,29 +1,71 @@
 import React from 'react';
+import { graphql, QueryRenderer } from "react-relay";
+
+import _ from 'lodash';
+import environment from "../../environment";
 import TopBar from '../topbar/topbar';
 import Filter from '../Filter';
 import Dashboard from '../Dashboard';
 import Footer from '../Footer';
 import './home.scss';
 
-const Home = (props) => (
-  <div className="content">
-    <TopBar />
-    <div className="main-content">
-      <div className="content-headers">
-        <div className="filters-header">
-          <h1 className="header">Filters</h1>
+class Home extends React.Component {
+  state = {
+    searchTerm: ""
+  }
+  
+  handleSubmit = (searchTerm) => {
+    this.setState({ searchTerm });
+  };
+
+  delayedHandleSubmit = _.debounce(this.handleSubmit, 300);
+
+  render() {
+    const { searchTerm } = this.state;
+    const DashboardQueryRenderer = (
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query homeQuery ($searchTerm: String) {
+            allJobs(jobTitle_Icontains: $searchTerm, before: "12/3/2020") {
+              ...Dashboard_jobs
+            }
+          }
+        `}
+        variables={{ searchTerm }}
+        render={({ error, props }) => {
+          if (error) {
+            return <div>Error!</div>;
+          }  
+          if (props) {
+            return <Dashboard jobs={props.allJobs} />;
+          }
+          return <div>Loading...</div>;
+        }}
+      />
+    );
+
+    return (
+      <div className="content">
+        <TopBar handleSearch={this.delayedHandleSubmit} />
+        <div className="main-content">
+          <div className="content-headers">
+            <div className="filters-header">
+              <h1 className="header">Filters</h1>
+            </div>
+            <div className="jobs-header">
+              <h1 className="header">All Jobs</h1>
+            </div>
+          </div>
+          <div className="content-group">
+            <Filter />
+            {DashboardQueryRenderer}
+          </div>
         </div>
-        <div className="jobs-header">
-          <h1 className="header">All Jobs</h1>
-        </div>
+        <Footer />
       </div>
-      <div className="content-group">
-        <Filter />
-        <Dashboard {...props} />
-      </div>
-    </div>
-    <Footer />
-  </div>
-);
+    );
+  }
+}
 
 export default Home;
