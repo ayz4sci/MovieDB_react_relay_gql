26fa5bec6f89c693ca5eca225d61267675a3f738
Ayoola Ajebeku <ayz4sci@yahoo.com>
2019-04-25 04:43:10 +0100

    refactor(ui-improvement): Improve UI

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -11,39 +11,37 @@ import PropTypes from 'prop-types';
 import "./filter.scss";
 
 const Filter = ({ handleGender, gender }) => (
-  <div>
-    <div>
-      <FormGroup check>
-        <Label check>
-          <Input 
-            type="checkbox" 
-            onChange={(event) => handleGender(event.target.value)} 
-            value="female" 
-            checked={gender === "female"} 
-          /> FEMALE
-        </Label>
-      </FormGroup>
-      <FormGroup check>
-        <Label check>
-          <Input 
-            type="checkbox" 
-            value="male" 
-            onChange={(event) => handleGender(event.target.value)} 
-            checked={gender === "male"} 
-          /> MALE
-        </Label>
-      </FormGroup>
-      <FormGroup check>
-        <Label check>
-          <Input 
-            type="checkbox" 
-            value="notspecified" 
-            onChange={(event) => handleGender(event.target.value)} 
-            checked={gender === "notspecified"} 
-          /> NOT SPECCIFIED
-        </Label>
-      </FormGroup>
-    </div>
+  <div className="filters">
+    <FormGroup check>
+      <Label check>
+        <Input 
+          type="checkbox" 
+          onChange={(event) => handleGender(event.target.value)} 
+          value="female" 
+          checked={gender === "female"} 
+        /> FEMALE
+      </Label>
+    </FormGroup>
+    <FormGroup check>
+      <Label check>
+        <Input 
+          type="checkbox" 
+          value="male" 
+          onChange={(event) => handleGender(event.target.value)} 
+          checked={gender === "male"} 
+        /> MALE
+      </Label>
+    </FormGroup>
+    <FormGroup check>
+      <Label check>
+        <Input 
+          type="checkbox" 
+          value="notspecified" 
+          onChange={(event) => handleGender(event.target.value)} 
+          checked={gender === "notspecified"} 
+        /> NOT SPECCIFIED
+      </Label>
+    </FormGroup>
   </div>
 );
   
